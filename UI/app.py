5d261330a7da02e0da10de8664c0ba4a7afa6a28
AmoushRye <amoskhams1234@gmail.com>
2024-04-05 00:46:42 +0530

    Model Integration Done!!!!

--- UI/app.py
+++ UI/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/')
+# Load the pre-trained model
+with open('../model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+def classify_review(review_text):
+    prediction = model.predict([review_text])
+    return prediction[0]
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        return render_template('result.html')  # Redirect to the result page after form submission
+    else:
+        return render_template('home.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    review_text = request.form['text']
+    prediction = classify_review(review_text)
+    if prediction == "CG":
+        main_prediction = "Fake"
+    else:
+        main_prediction = "Original"
+    return render_template('result.html', pred=main_prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
